e28319fa466e902d1dd94f706f1b1ca803f02e76
ralphvirtucio <ralphvirtucio3@gmail.com>
2022-06-27 11:29:16 +0800

    Feat: rating section

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-'user strict';
+'use strict';
+
+// sample javascript
+
+// const interactiveFeedback = document.querySelector('.interactive__feedback');
+// const interactiveThankyou = document.querySelector('.interactive__thankyou');
+// const clicked = () => {
+//   interactiveThankyou.classList.remove('hidden');
+// };
+
+// // const rateBtn = document.querySelectorAll('.interactive__cta-btn');
+
+// const submitBtn = document.querySelector('.interactive__submit-btn');
+
+// submitBtn.addEventListener('click', clicked);
+
+// // for (let i = 0; i < rateBtn.length; i++) {
+// //   rateBtn[i].addEventListener('click', clicked);
+// // }

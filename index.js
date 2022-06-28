c5f4ec5c3a1d9f9ac2e41a25313a593cae9f2be2
ralphvirtucio <ralphvirtucio3@gmail.com>
2022-06-28 22:14:46 +0800

    Give some feedback

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 'use strict';
 
-const interactiveRating = document.querySelector('.interactive__rating');
-const interactiveResult = document.querySelector('.interactive__thankyou');
-
+//Selected Elements
 const ratingBtn = document.querySelectorAll('.interactive__cta-btn');
 const submitBtn = document.querySelector('.interactive__submit-btn');
+const thankYouSection = document.querySelector('.interactive__thankyou');
+const welcomeSection = document.querySelector('.interactive__rating');
 
-let rating = 0;
-rating;
-
-const ratingClicked = i => {
-  i.classList.add('rating-clicked');
-  document.querySelector('.rating').textContent = i.value;
+//Main Logic
+const ratingClicked = rating => {
+  if (!rating.classList.contains('rating-clicked')) {
+    rating.classList.add('rating-clicked');
+    document.querySelector('.rating').textContent = rating.value;
+  } else {
+    rating.classList.remove('rating-clicked');
+    document.querySelector('.rating').textContent = 0;
+  }
 };
+
 ratingBtn.forEach(i => {
   i.addEventListener('click', () => ratingClicked(i));
 });
 
 submitBtn.addEventListener('click', () => {
-  interactiveRating.classList.add('hidden');
-  interactiveResult.classList.remove('hidden');
+  thankYouSection.classList.remove('hidden');
+  welcomeSection.classList.add('hidden');
 });

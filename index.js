9d98838bd0c39f2be7e76f2ececa63422f15d2e5
ralphvirtucio <ralphvirtucio3@gmail.com>
2022-12-28 23:04:47 +0800

    Implemented: Mobile Design 1. WIP - dynamic page 2. Implemented both mobile design state

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// alert('Hello World');
+// WORK IN PROGRESS
+
+const ratingBtn = document.querySelectorAll('input[name="rating"]');
+const submitBtn = document.querySelector('input[name="submit"]');
+const rating = document.querySelector('.rating');
+const isShow = document.querySelector('.interactive-ratings--state');
+const isHidden = document.querySelector('.interactive-thankyou-state');
+
+submitBtn.addEventListener('click', function () {
+  ratingBtn.forEach(rate => {
+    if (rate.checked) {
+      rating.textContent = rate.value;
+      isHidden.classList.remove('hidden');
+      isShow.classList.add('hidden');
+    }
+  });
+});

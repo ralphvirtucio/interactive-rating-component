d713184d426475ea96466998f2dcd6e43adc6252
ralphvirtucio <ralphvirtucio3@gmail.com>
2022-12-29 15:52:26 +0800

    WIP

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// WORK IN PROGRESS
+// // WORK IN PROGRESS
 
 const ratingBtn = document.querySelectorAll('input[name="rating"]');
+const rating = document.querySelector('.rate');
 const submitBtn = document.querySelector('input[name="submit"]');
-const rating = document.querySelector('.rating');
 const isShow = document.querySelector('.interactive-ratings--state');
 const isHidden = document.querySelector('.interactive-thankyou-state');
 
-submitBtn.addEventListener('click', function () {
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault();
   ratingBtn.forEach(rate => {
     if (rate.checked) {
       rating.textContent = rate.value;

c87aacfe028e1e2719ce7f39df30897ede5e580c
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-01-02 23:42:58 +0800

    Updated bg color and padding of submit button

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const isHidden = document.querySelector('.interactive-thankyou-state');
 submitBtn.addEventListener('click', function (e) {
   // Prevent on not showing thank you card component
   e.preventDefault();
+
   // Iterate through the nodes of input type=radio
-  ratingBtn.forEach(rate => {
+  ratingBtn.forEach((rate) => {
     // Check if the radio/rate is checked
     if (rate.checked) {
       // Assign the rate value to the rating text content
